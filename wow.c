f8ec782eb2c13032fee633d239b29f6f5b54aa55
ankushshaw764 <ankushshaw764@gmail.com>
2025-09-26 16:15:36 +0530

    added factorial feature

--- wow.c
+++ wow.c
@@ -1,2 +1,27 @@
 //hii ths is a test file for auto commit through vs code
 //hii
+// program for factorial of a number
+#include <stdio.h>
+int fact(int x)
+{
+  int factorial = 1;
+  if (x == 0 || x == 1)
+  {
+    return 1;
+  }
+  for (int i = 1; i <= x; i++)
+  {
+    factorial = factorial * i;
+  }
+  return factorial;
+}
+
+int main()
+{
+  int num;
+  printf("Enter the number : ");
+  scanf("%d", &num);
+  printf("The factorial of '%d' is : %d", num, fact(num));
+
+  return 0;
+}
